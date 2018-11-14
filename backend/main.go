0a04c0e5fa109a3a5c30fe6ab3819f3018d9def5
ken5scal <kengoscal@gmail.com>
2018-11-15 02:13:33 +0900

    udpate

--- backend/main.go
+++ backend/main.go
@@ -1,4 +1,83 @@
 package main
 
+import (
+	"net/http"
+	"io"
+	"log"
+	"fmt"
+	"math/rand"
+	"github.com/gorilla/mux"
+)
+
 func main() {
+	// HandlerFunc is a method of multiplexer (ServerMux)
+	// Handler implements ServeHTTP(ResponseWriter, *Request)
+	http.HandleFunc("/hello", myServer)
+
+	//an HTTP request multiplexer
+	_ = &CustomServeMux{} //mux := &CustomServeMux{}
+
+	// ServerMux also implements ServeHTTP(ResponseWriter, *Request)
+	newMux := http.NewServeMux()
+	newMux.HandleFunc("/randomFloat", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, rand.Float64())
+	})
+	newMux.HandleFunc("/randomInt", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, rand.Intn(100))
+	})
+
+	// Gorilla Mux
+	r := mux.NewRouter()
+	s := r.PathPrefix("/articles").Subrouter()
+	s.HandleFunc("/{category}/{id:[0-9]+}", ArticleHandler).Name("articleRoute")
+	//r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler).Name("articleRoute")
+	r.PathPrefix("/articles").HandlerFunc(ArticleHandler)
+	r.StrictSlash(true)
+	r.HandleFunc("/", QueryHandler).Methods(http.MethodGet)
+	r.Queries("id", "category")
+	//r.PathPrefix("/articles/").HandlerFunc(ArticleHandler)
+	r.UseEncodedPath()
+	s.UseEncodedPath()
+	url, _ := s.Get("articleRoute").URL("category", "books", "id", "123")
+	fmt.Printf(url.String())
+
+	srv := &http.Server{
+		Handler: r,//newMux,
+		Addr: "127.0.0.1:8000",
+	}
+
+	log.Fatalln(srv.ListenAndServe())
+}
+
+func QueryHandler(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Got parameter id:%s!\n", queryParams["id"])
+	fmt.Fprintf(w, "Got parameter category:%s!", queryParams["category"])
+}
+
+func ArticleHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category is: %v\n", vars["category"])
+	fmt.Fprintf(w, "ID is: %v\n", vars["id"])
+}
+
+func myServer(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello world\n")
+}
+
+type CustomServeMux struct {}
+func (c *CustomServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path == "/" {
+		giveRandom(w, r)
+		return }
+	http.NotFound(w, r)
+	return
+}
+
+func giveRandom(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Your random number is: %f", rand.Float64())
 }
