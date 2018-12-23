950fa49d85cd71d50f5ba70786fe4edc5644fe02
ken5scal <kengoscal@gmail.com>
2018-12-24 03:36:35 +0900

    implemented android safety attestation offlive verification

--- backend/attestation.go
+++ backend/attestation.go
@@ -1,14 +1,11 @@
 package main
 
-type AndroidSafetyNetAttestationResponsePayload struct {
+type AndroidSafetyNetAttestationResponse struct {
 	Nonce                      string   `json:"nonce"`
 	TimestampMs                int64    `json:"timestampMs"`
 	ApkPackageName             string   `json:"apkPackageName"`
 	ApkCertificateDigestSha256 []string `json:"apkCertificateDigestSha256"`
-	ApkDigestSha256            []string `json:"apkDigestSha256"`
-}
-
-type AndroidSafetyNetAttestationHeader struct {
-	Alg string   `json:"alg"`
-	X5C []string `json:"x5c"`
+	ApkDigestSha256            string `json:"apkDigestSha256"`
+	CtsProfileMatch            bool   `json:"ctsProfileMatch"`
+	BasicIntegrity            bool   `json:"basicIntegrity"`
 }
